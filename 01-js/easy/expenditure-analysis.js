1f6e899bb7f8394269e3ab216b83f8c9cadea9d8
Mohtasim Billah <mohtasimbillah62@gmail.com>
2024-01-29 20:29:32 -0500

    expenditure analysis done

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,15 +14,36 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
+
+  if (transactions.length === 0) {
+    return [];
+  }
+  
   result = [];
 
   categoryObj = {};
 
-  for(transaction in transactions) {
-    categoryObj
+  for (let transaction of transactions) {
+    // Get the category of current item.
+    const category = transaction.category;
+    // If the category is repeated, increment the total amount.
+    if (categoryObj [category]) {
+      categoryObj[category] += transaction.price;
+    } else {
+      // If the category comes first time, update the amount with current price.
+      categoryObj[category] = transaction.price;
+    }
+  }
+
+  for (let category in categoryObj) {
+    result.push({
+      category: category,
+      totalSpent: categoryObj[category],
+    });
   }
 
   return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
+
