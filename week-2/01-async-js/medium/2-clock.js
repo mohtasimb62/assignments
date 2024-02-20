5d8f8ae1adaba98421f80b3567bb098a6146856c
Mohtasim Billah <mohtasimbillah62@gmail.com>
2024-02-19 19:23:44 -0500

    async medium 2-clock done

--- week-2/01-async-js/medium/2-clock.js
+++ week-2/01-async-js/medium/2-clock.js
@@ -10,10 +10,24 @@ function showTime() {
 }
 
 function showTimeAMPM() {
-    
+    const time = new Date();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+
+    let ampm;
+    if (hours >= 12) {
+        ampm = "PM";
+        hours = hours % 12;
+    } else {
+        ampm = "AM";
+    }
+
+    console.log(`${hours} : ${minutes} : ${seconds} ${ampm}`)
 }
 
 setInterval(function() {
     console.clear();
     showTime();
-}, 1000);
+    showTimeAMPM();
+}, 5000);
