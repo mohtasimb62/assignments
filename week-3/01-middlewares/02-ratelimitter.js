5971dc986dc1939508198489f8e1912301538902
Mohtasim Billah <mohtasimbillah62@gmail.com>
2024-02-19 20:29:12 -0500

    01-middlewares 02-ratelimitter done

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,22 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req, res, next) => {
+  const userID = req.headers["user-id"];
+
+  if (numberOfRequestsForUser[userID]) {
+    numberOfRequestsForUser[userID]++;
+    if (numberOfRequestsForUser[userID] > 5) {
+      res.status(404).send("NO ENTRY");
+    } else {
+      next();
+    }
+  } else {
+    numberOfRequestsForUser[userID] = 1;
+    next();
+  }
+});
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
